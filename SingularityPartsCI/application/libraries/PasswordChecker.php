76e3a3de3888e50b1083c74cfe47ca2a28ed7624
logan.widick@gmail.com <logan.widick@gmail.com@a63da5e1-a732-6986-d695-c1f065669087>
2013-11-18 15:34:59 +0000

    Added pseudocode for adding a user, and validating a password. Does not include validation or the forms.

---
+++ SingularityPartsCI/application/libraries/PasswordChecker.php
@@ -0,0 +1,53 @@
+
+<?php 
+class PasswordChecker{
+
+/* ================= BEGIN CONSTANTS ========================================*/
+	//combos per second supercomputer
+	protected $COMBOS_PER_SECOND = 100000000000000000;
+	
+	/*
+	 * ok, so our typical attacker is not going to be a supercomputer.
+	 * This is how many times slower the average attacker will be.
+	 * For example, if the average attacker has 1/16 the computing resources, 
+	 * you set this value to 16. 
+	 */
+	protected $AVERAGE_STRENGTH_COMPARISON = 16;
+	
+	//valid passwords MUST NOT be cracked by the average attacker in less time than this.
+	protected $REQUIRED_MIN_DAYS = 90;
+/* ================= END CONSTANTS ========================================*/
+
+	
+   /**
+	* Validates the password 
+	*/
+	protected function isStrongEnoughPassword($pass)
+	{
+		$days_to_crack = checkStrength($pass);
+		if($days_to_crack < $required_days)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * Returns the number of days the average attacker will take to crack the password.
+	 */
+	protected function checkStrength($pass)
+	{
+		$pass_array = str_split($pass);
+		$pass_unique = array_unique($pass_array);
+		$alphabet_size = count($pass_unique);
+		$pass_length = count($pass_array);
+		$total_combos = pow($alphabet_size, $pass_length);
+		
+		//predict number of days to cracking
+		$seconds_to_crack = $total_combos / $combos_sec;
+		$days_to_crack = $seconds_to_crack / 3600 / 24;
+		
+		return round($days_to_crack * $avg_strength);
+	}
+}
+?>
---
+++ SingularityPartsCI/application/models/user_model.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * At this point this class is pseudocode for inserting a new user. 
+ * It performs no validation, form creation, etc.
+ */
+class User_Model extends CI_Model {
+
+		/**
+		 * This is pseudocode for creating a user.
+		 * Form inputs (REQUIRED unless stated otherwise):
+		 * Date of birth (REQUIRED; values returned by post MUST be numbers)
+		 * month - can be select box or input box
+		 * day - can be select box or input box
+		 * year - can be select box or input box
+		 *
+		 * Email address (REQUIRED):
+		 * email_address - an input box for the address
+		 * email_type - a combo box with contact_info_type information. 
+		 *	Values returned by post MUST be numeric (the id), 
+		 *	but the information displayed to the user should be the name of the contact type.
+		 *
+		 * Address (all input boxes except for state and address_type):
+		 * street_no 
+		 * street_name 
+		 * apt_or_suite_no (NOT required)
+		 * city
+		 * state - should display State,Country to the user; 
+			values returned by post MUST be numeric (the ids from state table)				
+		 * postcode
+		 * po_box (NOT required)
+		 * address_type - same as email_type
+		 *
+		 * phone number:
+		 * phone_number: similar to email_address
+		 * phone_type: same as email_type
+		 */
+		 function create_user(){ 
+			
+			//DOB
+			$mth = $this->input->post('month');
+			$day = $this->input->post('day');
+			$year = $this->input->post('year');
+			//TODO: check that this is right. 
+			$dob = strtotime($mth . '/' . $day . '/'. $year);
+			
+			//total person data
+			$person_data = array(
+				'first_name' => $this->input->post('first_name'),
+				'user_name' => mb_tolower($this->input->post('user_name')),
+				'last_name' => $this->input->post('last_name'),
+				'middle_name' => $this->input->post('middle_name'),
+				'dob' => date('Y-m-d', $dob),
+			);
+			
+			//insert person
+			$this->db->insert('person', $person_data);
+			$person_id = $this->db->insert_id();
+			
+			//password 
+			$clear_pass = $this->input->post('password');
+			$hashed_pass = password_hash($clear_pass, PASSWORD_DEFAULT);
+			$start_date = strtotime("now");
+			$days_to_expire = $this->PasswordChecker->checkStrength($clear_pass);
+			$end_date = strtotime("+$days_to_expire days");
+			$password_data = array(
+				'person_id' => $person_id,
+				'start_date' => date('Y-m-d', $start_date),
+				'end_date' => date('Y-m-d', $end_date),
+				'hash' => $hashed_pass,
+			);
+			$this->db->insert('password_hash', $password_data);
+			
+			//email
+			$email_data = array(
+				'email' => $this->input->post('email_address'),
+				'contact_info_type_id' => $this->input->post('email_type'),
+			);
+			$this->db->insert('email_address', $email_data);
+			$email_id = $this->db->insert_id();
+			
+			//email join table
+			$email_join_data = array(
+				'email_address_id' => $email_id,
+				'person_id' => $person_id,
+			);
+			$this->db->insert('email_address_person', $email_join_data);
+			
+			//address
+			$addr_data = array(
+				'street_no' => $this->input->post('street_no'),
+				'street_name' => $this->input->post('street_name'),
+				'apt_or_suite_no' => $this->input->post('apt_or_suite_no'),
+				'city' => $this->input->post('city'),
+				'state' => $this->input->post('state'),
+				'postcode' => $this->input->post('postcode'),
+				'po_box' => $this->input->post('street_no'),
+				'contact_info_type_id' => $this->input->post('address_type'),
+			);
+			$this->db->insert('address', $addr_data);
+			$address_id = $this->db->insert_id();
+			
+			//address join table
+			$address_join_data = array(
+				'address_id' => $address_id,
+				'person_id' => $person_id,
+			);
+			$this->db->insert('address_person', $address_join_data);
+			
+			//phone number
+			$phone_data = array(
+				'phone_number' => $this->input->post('phone_number'),
+				'contact_info_type_id' => $this->input->post('phone_type'),
+			);
+			$this->db->insert('phone_number', $phone_data);
+			$phone_id = $this->db->insert_id();
+			
+			//phone join table
+			$phone_join_data = array(
+				'phone_number_id' => $phone_id,
+				'person_id' =>$person_id,
+			);
+			$this->db->insert('person_phone_number', $phone_join_data);
+			
+			//TODO: return stuff?
+		} 
+
+}	
+?>
