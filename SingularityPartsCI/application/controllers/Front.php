84d30c8bcb9cb5c50eb7d199373667300ad00477
logan.widick@gmail.com <logan.widick@gmail.com@a63da5e1-a732-6986-d695-c1f065669087>
2013-11-24 19:16:38 +0000

    fixed both login and logout bugs?

--- SingularityPartsCI/application/controllers/Front.php
+++ SingularityPartsCI/application/controllers/Front.php
@@ -1,42 +1,35 @@
 <?php
 	class Front extends CI_controller{
 
-	function __construct()
-    {
-        parent::__construct();
- 
-        /* Standard Libraries of codeigniter are required */
-        $this->load->database();
-        $this->load->helper('url');
-        /* ------------------ */ 
- 
- 
-	}
-	
-	function load_stuff()
+	function loadStuff()
 	{
+		//load helpers
 		$this->load->helper('form');
 		$this->load->helper('html');
 		$this->load->helper('url');
 		$this->load->model('Front_model');
-			
+		$this->load->library('form_validation');
+		
+		//load views
 		$data['result'] = $this->Front_model->getData();
 		$data['page_title'] = "Singularity Parts";
-
 		$this->load->view('Front_view',$data);
 	}
 	
 	function index()
 	{
-		$this->load_stuff();
-    }
+		$this->loadStuff();
+		
+   }
 	function login()
 	{
-		$this->load_stuff();
-
+		$this->loadStuff();
+		
+		//set form validation rules
 		$this->form_validation->set_rules('Email','Email','required');
         $this->form_validation->set_rules('Password','Password','required');
 				
+		//apply validation rules
 		if($this->form_validation->run() === FALSE)
         {					
             $this->load->view('Register');
@@ -51,7 +44,14 @@
 	
 	function logout()
 	{
-		$this->load_stuff();
+		//load helpers
+		$this->loadStuff();
+			
+		//load front view
+		$data['result'] = $this->Front_model->getData();
+		$data['page_title'] = "Singularity Parts";
+		$this->load->view('Front_view',$data);
+		
 		$this->Front_model->logout();
 	}
 }
