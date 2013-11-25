117befc9eb2141f15e50b36b49c83191f55a8c9c
logan.widick@gmail.com <logan.widick@gmail.com@a63da5e1-a732-6986-d695-c1f065669087>
2013-11-25 14:41:07 +0000

    Changed config to wipe out sessions on close.

--- SingularityPartsCI/application/config/config.php
+++ SingularityPartsCI/application/config/config.php
@@ -245,9 +245,9 @@ $config['encryption_key'] = 'SJwZMHhKNZbKrCFfPexM65y3';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
-$config['sess_encrypt_cookie']	= FALSE;
+$config['sess_expiration']		= 3600;
+$config['sess_expire_on_close']	= TRUE;
+$config['sess_encrypt_cookie']	= FALSE; //set to TRUE before turning in.
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
@@ -268,7 +268,7 @@ $config['sess_time_to_update']	= 300;
 $config['cookie_prefix']	= "";
 $config['cookie_domain']	= "";
 $config['cookie_path']		= "/";
-$config['cookie_secure']	= FALSE;
+$config['cookie_secure']	= TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -279,7 +279,7 @@ $config['cookie_secure']	= FALSE;
 | COOKIE data is encountered
 |
 */
-$config['global_xss_filtering'] = FALSE;
+$config['global_xss_filtering'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -293,7 +293,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = FALSE;
+$config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
