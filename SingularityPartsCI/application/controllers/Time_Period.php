4579b76a0eda37d7bd9236f35ed0321b3a2ada03
logan.widick@gmail.com <logan.widick@gmail.com@a63da5e1-a732-6986-d695-c1f065669087>
2013-11-26 19:55:56 +0000

    Finished copy-paste things for store mode crud views (except for order cancellation, order shipping)

---
+++ SingularityPartsCI/application/controllers/Time_Period.php
@@ -0,0 +1,97 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Time_Period extends CI_Controller {
+ 
+	private $hasFront = false;
+	function __construct()
+    {
+        parent::__construct();
+ 
+        //load stuff
+        $this->load->database();
+		$this->load->helper('form');
+		$this->load->helper('html');
+		$this->load->helper('url');
+        $this->load->library('grocery_CRUD');
+		
+		//show stuff
+		$this->showFront();
+    }
+	
+	/**
+	 * Returns true if we have access.
+	 */
+	public static function has_access()
+	{
+		$CI = get_instance();
+		if(!$CI->session->userdata('person_id') || 
+			!$CI->session->userdata('logged_in')) return false;
+		$CI->load->model('RBAC_model');
+		if(!$CI->RBAC_model->has_permission(
+				$CI->session->userdata('person_id'), 
+				'time_period',
+				array(	
+					'role_permission.can_read' => TRUE, 
+					'role_permission.can_add'=>TRUE,
+					'role_permission.can_modify' => TRUE,
+					'role_permission.can_delete' => TRUE,
+					
+				)
+			)
+		)
+		{
+			return false;
+		}
+		else return true;
+	}
+	
+	/**
+	 * Returns a printable name for the controller.
+	 */ 
+	public static function get_controller_name()
+	{
+		return 'Manage Time Periods';
+	}
+	
+	public static function is_store_mode()
+	{
+		return TRUE;
+	}
+	
+	function showFront()
+	{
+		include_once('Dashboard.php');
+		if($this->hasFront) return;
+		
+		//load stuff
+		$this->load->model('Front_model');
+		
+		//show stuff
+		$data['result'] = $this->Front_model->getData();
+		$data['customer_mode'] = Dashboard::is_mode_ok(FALSE);
+		$data['store_mode'] = Dashboard::is_mode_ok(TRUE);
+		$data['page_title'] = "Singularity Parts";
+		$this->load->view('Front_view',$data);
+		
+		$this->hasFront = true;
+	}
+	
+    function index()
+    {
+		$this->load->library('Grocery_CRUD');
+		$this->load->model('RBAC_model');
+		$this->showFront();
+
+		//do we have access?
+		if(!self::has_access()) redirect('');
+		
+		//load view
+		$this->load->library('Grocery_CRUD');
+        $this->grocery_crud->set_table('time_period');
+        $output = $this->grocery_crud->render();
+		$this->load->view('grocery_crud_view',$output);
+
+		
+    }
+}
+ 
